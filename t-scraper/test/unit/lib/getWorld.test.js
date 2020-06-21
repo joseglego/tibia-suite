19d49c7d67b29f28a8a92e7e9fb6f829bd95ab02
Jose Lezama <joseglego@gmail.com>
2020-06-21 19:25:00 -0400

    test(getWorld): define stub and test for getWorld

---
+++ t-scraper/test/unit/lib/getWorld.test.js
@@ -0,0 +1,33 @@
+const tabletojson = require('tabletojson').Tabletojson
+
+const getWorld = require('../../../src/libs/getWorld')
+const worldMock = require('../../stubs/world.json')
+
+jest.mock('tabletojson')
+
+describe('getWorld', () => {
+  it('get world page details', async () => {
+    tabletojson.convertUrl.mockResolvedValue(worldMock)
+
+    const world = await getWorld()
+    expect(world)
+      .toStrictEqual({
+        worldInfo: {
+          status: 'Online',
+          playersOnline: '624',
+          onlineRecord: '1,055 players (on May 01 2020, 17:58:30 CEST)',
+          creationDate: '01/97',
+          location: 'Europe',
+          pvPType: 'Open PvP',
+          worldQuestTitles: "Rise of Devovorga, The Lightbearer, The Colours of Magic, A Piece of Cake, Demon's Lullaby",
+          battlEyeStatus: 'Protected by BattlEye since August 29, 2017.',
+          gameWorldType: 'Regular'
+        },
+        onlineCharacters: [
+          { name: 'Aaellfous Lubilto', level: 8, vocation: 'Sorcerer' },
+          { name: 'Abel Paladin Mocny', level: 27, vocation: 'Paladin' },
+          { name: 'Abuyin ibn Djadir', level: 34, vocation: 'Paladin' }
+        ]
+      })
+  })
+})
