d370f9a8bd3b603741fda9d276d1c9534ec6fd20
Jose Lezama <joseglego@gmail.com>
2020-06-22 00:38:22 -0400

    test(getWorlds): tests works even when there are no tournamentWorlds

--- t-scraper/test/unit/lib/getWorlds.test.js
+++ t-scraper/test/unit/lib/getWorlds.test.js
@@ -2,7 +2,7 @@ const tabletojson = require('tabletojson').Tabletojson
 
 const getWorlds = require('../../../src/libs/getWorlds')
 const worldsMock = require('../../stubs/worlds.json')
-
+const worldsWithoutTournamentMock = require('../../stubs/worldsWithoutTournament.json')
 jest.mock('tabletojson')
 
 describe('getWorlds', () => {
@@ -20,6 +20,21 @@ describe('getWorlds', () => {
         tournamentWorlds: [
           { world: 'Endebra', online: 0, location: 'South America', pvpType: 'Open PvP', additionalInfo: 'premium, blocked, restricted Store products' }
         ]
+      })
+  })
+
+  it('get worlds list information without tournamentworlds', async () => {
+    tabletojson.convertUrl.mockResolvedValue(worldsWithoutTournamentMock)
+
+    const worlds = await getWorlds()
+    expect(worlds)
+      .toStrictEqual({
+        worlds: [
+          { world: 'Antica', online: 826, location: 'Europe', pvpType: 'Open PvP', additionalInfo: '' },
+          { world: 'Assombra', online: 88, location: 'South America', pvpType: 'Retro Hardcore PvP', additionalInfo: '' },
+          { world: 'Astera', online: 342, location: 'North America', pvpType: 'Optional PvP', additionalInfo: '' }
+        ],
+        tournamentWorlds: []
       })
   })
 })
