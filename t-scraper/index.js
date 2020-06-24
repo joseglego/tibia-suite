e9e4cf7044f5c02af7daddc0f6e57570365ea52b
Jose Lezama <joseglego@gmail.com>
2020-06-23 23:29:18 -0400

    feat(index): define index.js following good practices

---
+++ t-scraper/index.js
@@ -0,0 +1,11 @@
+const getCharacter = require('./src/lib/getCharacter')
+const getWorlds = require('./src/lib/getWorlds')
+const getWorld = require('./src/lib/getWorld')
+const getGuilds = require('./src/lib/getGuilds')
+const getGuild = require('./src/lib/getGuild')
+
+exports.getCharacter = getCharacter
+exports.getWorlds = getWorlds
+exports.getWorld = getWorld
+exports.getGuilds = getGuilds
+exports.getGuild = getGuild
