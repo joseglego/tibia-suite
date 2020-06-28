d2dbc47a483df2c799bdbf7f3ec5d190fa1493b4
Jose Lezama <joseglego@gmail.com>
2020-06-28 17:19:00 -0400

    feat(character): new version with whole info character

--- packages/cli/src/commands/character.js
+++ packages/cli/src/commands/character.js
@@ -1,8 +1,11 @@
 const { Command, flags } = require('@oclif/command')
 const { CLIError } = require('@oclif/errors')
-const cli = require('cli-ux')
+const { cli } = require('cli-ux')
 
 const { getCharacter } = require('@tibia-suite/scraper')
+
+const jsonToTable = require('../utils/json-to-table')
+const drawTable = require('../utils/draw-table')
 
 class CharacterCommand extends Command {
   async run () {
@@ -16,7 +19,15 @@ class CharacterCommand extends Command {
     cli.action.start('starting a process')
     const character = await getCharacter(name)
     cli.action.stop()
-    this.log(character)
+
+    drawTable('Character Information', ...jsonToTable(character.characterInfo))
+
+    if (!minifiedInfo) {
+      drawTable('Achievements', ...character.achievements.map(achievement => [achievement]))
+      drawTable('Deaths', ...character.deaths.map(death => [death.date, death.description]))
+      drawTable('Account Information', ...jsonToTable(character.accountInfo))
+      drawTable('Characters', ...character.characters.map(character => ([character.name, character.world])))
+    }
   }
 }
 
@@ -24,7 +35,7 @@ CharacterCommand.description = 'Get the information from the character at tibia.
 
 CharacterCommand.flags = {
   name: flags.string({ char: 'n', description: 'name character to get data' }),
-  minified: flags.boolean({ char: 'm', description: 'show only character info without extend data' }),
+  minified: flags.boolean({ char: 'm', description: 'show only character info without extend data' })
 }
 
 module.exports = CharacterCommand
