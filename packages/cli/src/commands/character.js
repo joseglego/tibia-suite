d37cc578c53efb000a0567147cca165f09f51f68
Jose Lezama <joseglego@gmail.com>
2020-06-28 17:16:43 -0400

    feat(cli-ux): include cli.action.loading

--- packages/cli/src/commands/character.js
+++ packages/cli/src/commands/character.js
@@ -1,18 +1,21 @@
 const { Command, flags } = require('@oclif/command')
 const { CLIError } = require('@oclif/errors')
+const cli = require('cli-ux')
 
 const { getCharacter } = require('@tibia-suite/scraper')
 
 class CharacterCommand extends Command {
   async run () {
     const { flags } = this.parse(CharacterCommand)
-    const name = flags.name
+    const { name, minified: minifiedInfo } = flags
 
     if (!name) {
       throw new CLIError('name is required to get information')
     }
 
+    cli.action.start('starting a process')
     const character = await getCharacter(name)
+    cli.action.stop()
     this.log(character)
   }
 }
@@ -20,7 +23,8 @@ class CharacterCommand extends Command {
 CharacterCommand.description = 'Get the information from the character at tibia.com'
 
 CharacterCommand.flags = {
-  name: flags.string({ char: 'n', description: 'name character to get data' })
+  name: flags.string({ char: 'n', description: 'name character to get data' }),
+  minified: flags.boolean({ char: 'm', description: 'show only character info without extend data' }),
 }
 
 module.exports = CharacterCommand
