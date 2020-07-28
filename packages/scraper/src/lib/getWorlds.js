79c536a5a6cd04cfdc39d238095c65b946d57cb3
Jose Lezama <joseglego@gmail.com>
2020-07-27 22:05:26 -0400

    feat(getWorlds): rename and include attributes (name/battleEye)

--- packages/scraper/src/lib/getWorlds.js
+++ packages/scraper/src/lib/getWorlds.js
@@ -1,15 +1,23 @@
 const cheerio = require('cheerio')
 const fetchHTML = require('../utils/fetchHTML')
 
+const parseBattleEye = srcImg => {
+  if (srcImg.includes('icon_battleye.gif')) { return 'protected' }
+  if (srcImg.includes('icon_battleyeinitial.gif')) { return 'initial' }
+
+  return ''
+}
+
 const parseWorld = (htmlString) => {
   const $ = cheerio.load(`<table><tr>${htmlString}</tr></table>`)
 
   return ({
-    world: $('td:nth-of-type(1)').text(),
-    online: $('td:nth-of-type(2)').text() !== '-' ? +$('td:nth-of-type(2)').text() : 0,
+    additionalInfo: $('td:nth-of-type(6)').text().trim(),
+    battleEye: parseBattleEye($('td:nth-of-type(5)').html()),
     location: $('td:nth-of-type(3)').text(),
-    pvpType: $('td:nth-of-type(4)').text(),
-    additionalInfo: $('td:nth-of-type(6)').text().trim()
+    name: $('td:nth-of-type(1)').text(),
+    online: $('td:nth-of-type(2)').text() !== '-' ? +$('td:nth-of-type(2)').text() : 0,
+    pvpType: $('td:nth-of-type(4)').text()
   })
 }
 
