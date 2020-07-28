cef7f3df90d8999b080682e8ae01b256f0147842
Jose Lezama <joseglego@gmail.com>
2020-07-28 03:31:51 -0400

    feat(getWorld): define new version with cheerios

--- packages/scraper/test/unit/lib/getWorld.test.js
+++ packages/scraper/test/unit/lib/getWorld.test.js
@@ -1,32 +1,33 @@
-const tabletojson = require('tabletojson').Tabletojson
+const fetchHTML = require('../../../src/utils/fetchHTML')
 
 const getWorld = require('../../../src/lib/getWorld')
-const worldMock = require('../../stubs/world.json')
+const worldPage = require('../../stubs/worldPage')
 
-jest.mock('tabletojson')
+jest.mock('../../../src/utils/fetchHTML')
 
 describe('getWorld', () => {
   it('get world page details', async () => {
-    tabletojson.convertUrl.mockResolvedValue(worldMock)
+    fetchHTML.mockResolvedValue(worldPage)
 
     const world = await getWorld()
     expect(world)
       .toStrictEqual({
         worldInfo: {
           status: 'Online',
-          playersOnline: '624',
+          playersOnline: '165',
           onlineRecord: '1,055 players (on May 01 2020, 17:58:30 CEST)',
           creationDate: '01/97',
           location: 'Europe',
           pvPType: 'Open PvP',
-          worldQuestTitles: "Rise of Devovorga, The Lightbearer, The Colours of Magic, A Piece of Cake, Demon's Lullaby",
+          worldQuestTitles: "Rise of Devovorga, The Lightbearer, The Colours of Magic, Bewitched, A Piece of Cake, Demon's Lullaby",
           battlEyeStatus: 'Protected by BattlEye since August 29, 2017.',
           gameWorldType: 'Regular'
         },
         onlineCharacters: [
-          { name: 'Aaellfous Lubilto', level: 8, vocation: 'Sorcerer' },
-          { name: 'Abel Paladin Mocny', level: 27, vocation: 'Paladin' },
-          { name: 'Abuyin ibn Djadir', level: 34, vocation: 'Paladin' }
+          { name: 'Aandr', level: 334, vocation: 'Elder Druid' },
+          { name: 'Aegon Zaru', level: 70, vocation: 'Elder Druid' },
+          { name: 'Akatamachitos', level: 109, vocation: 'Elite Knight' },
+          { name: 'Akavom', level: 457, vocation: 'Elder Druid' }
         ]
       })
   })
