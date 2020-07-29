79194f3a1f87c675e90a54ae39df07404fea6882
Jose Lezama <joseglego@gmail.com>
2020-07-28 23:40:07 -0400

    feat(getGuild): define new version with cheerios

--- packages/scraper/test/unit/lib/getGuild.test.js
+++ packages/scraper/test/unit/lib/getGuild.test.js
@@ -1,70 +1,17 @@
-const tabletojson = require('tabletojson').Tabletojson
-
+const fetchHTML = require('../../../src/utils/fetchHTML')
 const getGuild = require('../../../src/lib/getGuild')
-const guildMock = require('../../stubs/guild.json')
-const guildWithoutInvitesMock = require('../../stubs/guildWithoutInvites.json')
-jest.mock('tabletojson')
+
+const guildPage = require('../../stubs/guildPage')
+const guildResponse = require('../../stubs/guildResponse.json')
+
+jest.mock('../../../src/utils/fetchHTML')
 
 describe('getGuild', () => {
   it('get guild list information', async () => {
-    tabletojson.convertUrl.mockResolvedValue(guildMock)
+    fetchHTML.mockResolvedValue(guildPage)
 
     const guild = await getGuild('Mock Guild')
     expect(guild)
-      .toStrictEqual({
-        guildInfo: 'Gildia PVP. Organizujemy tez teamhunty i questy dla czlonkow gildi. Teamspeak wymagany. W sprawie dolaczenia skontaktuj sie z ktoryms z liderow.\nThe guild was founded on Antica on Feb 13 2018.\nIt is currently active.\nGuild is opened for applications.',
-        members: [{
-          joined: 'Jan 26 2020',
-          level: 173,
-          name: 'Walefajans',
-          nick: 'Cool Nick',
-          rank: 'Test',
-          status: 'offline',
-          vocation: 'Master Sorcerer'
-        }, {
-          joined: 'May 05 2020',
-          level: 163,
-          name: 'Wareks Rozrabiaka',
-          nick: null,
-          rank: 'Test',
-          status: 'offline',
-          vocation: 'Master Sorcerer'
-        }, {
-          joined: 'Jun 03 2020',
-          level: 281,
-          name: 'Weeaboo Senpai',
-          nick: null,
-          rank: 'Test',
-          status: 'online',
-          vocation: 'Master Sorcerer'
-        }],
-        invitations: [{
-          name: 'Alkharnus',
-          invitationDate: 'Jun 14 2020'
-        }, {
-          name: 'Brius Dadir',
-          invitationDate: 'Jun 14 2020'
-        }]
-      })
-  })
-
-  it('handle no invitations guilds', async () => {
-    tabletojson.convertUrl.mockResolvedValue(guildWithoutInvitesMock)
-
-    const guild = await getGuild('Mock Guild')
-    expect(guild)
-      .toStrictEqual({
-        guildInfo: 'Gildia PVP. Organizujemy tez teamhunty i questy dla czlonkow gildi. Teamspeak wymagany. W sprawie dolaczenia skontaktuj sie z ktoryms z liderow.\nThe guild was founded on Antica on Feb 13 2018.\nIt is currently active.\nGuild is opened for applications.',
-        members: [{
-          joined: 'Jan 26 2020',
-          level: 173,
-          name: 'Walefajans',
-          nick: 'Cool Nick',
-          rank: 'Test',
-          status: 'offline',
-          vocation: 'Master Sorcerer'
-        }],
-        invitations: []
-      })
+      .toStrictEqual(guildResponse)
   })
 })
