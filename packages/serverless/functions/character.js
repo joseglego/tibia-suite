ce12d7ff61cec2d8da110fba545258afa0f3fcdb
Jose Lezama <joseglego@gmail.com>
2020-09-30 09:34:51 -0300

    refactor(character): use response helper

--- packages/serverless/functions/character.js
+++ packages/serverless/functions/character.js
@@ -1,17 +1,14 @@
 const scraper = require('@tibia-suite/scraper');
+const response = require('./utils/response');
 
 exports.handler = async ({ queryStringParameters }) => {
-  const NOT_FOUND = { statusCode: 404, body: 'Not found' };
   const {name} = queryStringParameters;
 
-  if (!name) { return NOT_FOUND; }
+  if (!name) { return response.notFound(); }
 
   const character = await scraper.getCharacter(name);
 
-  if (!character) { return NOT_FOUND; }
+  if (!character) { return response.notFound(); }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(character)
-  };
+  return response.json(character);
 };
