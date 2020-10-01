fa589ff3fd14042aa0b9fe89d31a054d5323b8f9
Jose Lezama <joseglego@gmail.com>
2020-09-30 21:48:48 -0300

    feat(guilds): accept guild name by path/query param

--- packages/serverless/functions/guilds.js
+++ packages/serverless/functions/guilds.js
@@ -2,7 +2,7 @@ const scraper = require('@tibia-suite/scraper');
 const response = require('./utils/response');
 
 exports.handler = async ({ path, queryStringParameters }) => {
-  const guildName = path.split('/')[3];
+  const guildName = path.split('/')[3] || queryStringParameters.name;
 
   if (guildName) {
     let guild;
