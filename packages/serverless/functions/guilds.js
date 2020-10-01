d861aa496ed2569b345ae3a834b1c9cad0282e82
Jose Lezama <joseglego@gmail.com>
2020-09-30 21:01:55 -0300

    feat(guildd(s)): include function for guilds/guild

---
+++ packages/serverless/functions/guilds.js
@@ -0,0 +1,31 @@
+const scraper = require('@tibia-suite/scraper');
+const response = require('./utils/response');
+
+exports.handler = async ({ path, queryStringParameters }) => {
+  const guildName = path.split('/')[4];
+
+  if (guildName) {
+    let guild;
+
+    try {
+      guild = await scraper.getGuild(guildName);
+    } catch(err) {
+      return response.notFound();
+    }
+
+    return response.json(guild);
+  } else {
+    const { world: worldName } = queryStringParameters;
+
+    if (!worldName) return response.badRequest('The world of the world is required');
+    let guilds;
+
+    try {
+      guilds = await scraper.getGuilds(worldName);
+    } catch(err) {
+      return response.internalError();
+    }
+
+    return response.json(guilds);
+  }
+};
